ffd24886a02ac55e1964b74b6cefae92066deafe
TheJPMZ <monzon.jpmz@gmail.com>
2022-05-17 18:49:07 -0600

    ➕Firebase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './App.scss';
+import firebase from "../firebase";
+import GetInfo from "./GetInfo";
+
+firebase.firestore().collection("carrito").onSnapshot(function(querySnapshot) {
+  querySnapshot.forEach(function(doc) {
+    console.log(doc.data());
+  });
+});
+
 
 function App() {
+
+    const addCompra = () => {
+        //Add something to the database
+        firebase.firestore().collection('carrito').add({
+            items: [{"imagen":"meme","precio":1,"nombre":"meme","cantidad":1}, {"imagen":"escopeta","precio":10,"nombre":"Escopeta","cantidad":1}],
+            usercode: '12345'
+        })
+    }
+
+
+
+    const memes = [1,2,3]
+
     return (
         <>
-            <h1>Hello World</h1>
+            <h1>Memes</h1>
+            <button onClick={addCompra}>Comprar</button>
+            <GetInfo />
+
         </>
     );
 }
