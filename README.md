284888bdcd47aed038e8e652867440fea97e113a
TheJPMZ <monzon.jpmz@gmail.com>
2022-05-17 20:00:13 -0600

    ♻ Fixed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Calculadora-react
+# Template 
+
+## Descripcion
+Es un template para trabajar en react sin utilizar react-create-app.
+
+### Instalacion
+Para instalar las dependencias es necesario correr:
+```bash
+$ npm install
+```
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,59 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.jsx",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "bundle_react"),
-  },
-  resolve: {
-    extensions: ["",".js", ".jsx"],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.m?js?x$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.(png|jpe?g|gif|mp4|mp3|webp|svg)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "[name][ext]",
-          outputPath: "assets/",
-          publicPath: "assets/",
-        },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
+    entry: './src/index.jsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?jsx$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                },
             },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true,
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
             },
-          },
+            {
+                test: /\.mp3$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'aud/',
+                    publicPath: 'aud/',
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif|mp4|mp3|webp|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: '[name][ext]',
+                    outputPath: 'assets/',
+                    publicPath: 'assets/',
+                },
+            },
         ],
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
     ],
-  },
 };
