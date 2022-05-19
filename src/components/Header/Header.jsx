9736ee46c1d2554c60a0a796be9c696c4e6f2988
TheJPMZ <monzon.jpmz@gmail.com>
2022-05-19 03:47:01 -0600

    ➕ Synced number with database

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,57 +1,67 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Badge, Button, AppBar, Toolbar, IconButton } from "@material-ui/core";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import Typography from "@mui/material/Typography";
+import firebase from "../../firebase";
 import "../App/App.scss";
 
 
 export default function Header() {
     const [count, setCount] = React.useState(0);
 
-      return (
-          <div>
-              <AppBar position="static" className="header">
-              <Toolbar className="header">
-              <Typography variant="h2"
-                component="div" sx={{ flexGrow: 1 }}>
-              </Typography>
+    //Todo: Add user
+        useEffect(() => {
+        firebase.firestore().collection("carrito").doc("ZsuFnGu76TWPQus6xGce").onSnapshot(snapshot => {
+            setCount(snapshot.data().items.length);
+        });
+    }, []);
 
-              <IconButton>
-              <Badge badgeContent = {count} showZero color="secondary" >
-                    <ShoppingCartIcon onClick={() => {
-                        console.log("open");}}
-                    />
-                </Badge>
+    if (count === 0) {
+        setCount(undefined)
+    }
 
-              </IconButton>
-              </Toolbar>
-              </AppBar>
+    return (
+    <div>
+        <AppBar position="static" className="header">
+            <Toolbar className="header">
+                <Typography variant="h2"
+                    component="div" sx={{ flexGrow: 1 }}>
+                </Typography>
 
-            <div>
+                <IconButton>
+                    <Badge badgeContent = {count} showZero color="secondary" >
+                        <ShoppingCartIcon onClick={() => {console.log("open");}}
+                        />
+                    </Badge>
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+
+        <div>
             <ButtonGroup>
-              <Button
-                aria-label="reduce"
-                onClick={() => {
-                  setCount(Math.max(count - 1, 0));
-                }}
-              >
-                <RemoveIcon fontSize="small" />
-              </Button>
-              <Button
-                aria-label="increase"
-                onClick={() => {
-                  setCount(count + 1);
-                }}
-              >
-                <AddIcon fontSize="small" />
-              </Button>
+                <Button
+                    aria-label="reduce"
+                    onClick={() => {
+                    setCount(Math.max(count - 1, 0));
+                    }}
+                    >
+                    <RemoveIcon fontSize="small" />
+                </Button>
+                    <Button
+                    aria-label="increase"
+                    onClick={() => {
+                    setCount(count + 1);
+                    }}
+                    >
+                    <AddIcon fontSize="small" />
+                </Button>
             </ButtonGroup>
-            </div>
-          </div>
+        </div>
+    </div>
 
 
-  );
+    );
 }
