735f13ce34bd72105bc454dc6be31cbc95f266b8
TheJPMZ <monzon.jpmz@gmail.com>
2022-05-19 03:27:39 -0600

    ➕ Made the button to add to cart better

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import firebase from "../../firebase";
 
-function AddToCart() {
+function AddToCart({imagen, precio, itemcode, nombre, cantidad_disponible}) {
 
     const addCompra = () => {
 
         firebase.firestore().collection('carrito').doc('ZsuFnGu76TWPQus6xGce').set(
             {items: firebase.firestore.FieldValue.arrayUnion( {
-                    "imagen":"https://i.dell.com/sites/imagecontent/products/PublishingImages/inspiron-15-7567-laptop/CS1703G0002-laptop-inspiron-15-7000-gaming-pdp-polaris-01.jpg",
-                    "precio":200,
-                    "itemcode":"AVPRfQAkWRx0m7OkmgZv",
-                    "nombre":"Laptop gaming Dell de 8GB de RAM, i7-7000, NVIDIA GTX",
-                    "cantidad":3} )},
+                    "imagen":imagen, //Str: url de imagen
+                    "precio":precio, //Int:200
+                    "itemcode":itemcode, //Str:AVPRfQAkWRx0m7OkmgZv
+                    "nombre":nombre, //Str: Laptop gaming Dell de 8GB de RAM, i7-7000, NVIDIA GTX,
+                    "cantidad":cantidad_disponible //Int:3
+            })},
             {merge: true}
         );
     }
